f4c2b1ba5e6ff4e9f58474ac5bbcda92dab11a5a
dziubek89 <dziubek151@interia.pl>
2023-04-02 16:45:07 +0200

    slider added

--- 01-dom-podstawy/js/main.js
+++ 01-dom-podstawy/js/main.js
@@ -3,16 +3,17 @@
 (() => {
     const button = document.getElementById('submit__button').addEventListener('click', (e) => {
         e.preventDefault()
-        const name = document.getElementById('name').value
-        const message = document.getElementById('message').value
+        const name = document.getElementById('name')
+        const message = document.getElementById('message')
         console.log(name, message)
         const ul = document.getElementById('messages__list')
 
         const li = document.createElement('li')
-        li.innerHTML = `<strong> ${name} </strong> napisal
-        <p> ${message} </p>`
+        li.innerHTML = `<strong> ${name.value} </strong> napisal
+        <p> ${message.value} </p>`
 
         ul.appendChild(li)
+
         name.value = ''
         message.value = ''
 
---
+++ 02-dom-slider/js/main.js
@@ -0,0 +1,62 @@
+
+// Napisz kod slidera
+
+// 1. Stworz sobie pusta tablice images
+// 2. Do tablicy images dodaj adresy zdjec pieskow z internetu
+//   -> Hint: aby wziac zdjecie pieska z internetu, wejdz na google images, kliknij prawy przycisk i wybierz Copy Image Adress
+// 3. Dodaj zmienna counter i ustaw jej wartosc poczatkowa na 0
+// 4. po zaladowaniu pliku JS, dodaj elementowi img atrybut src, odpowiadajacy pierwszemu elementowi tablicy images, zdefiniowanym w kroku 1
+// 5. po wcisnieciu przycisku next, zastap atrybut src obrazka nastepnym elementem z tablicy
+// 6. po wcisnieciu przycisku prev, zastap atrybut src obrazka poprzednim elementem z tablicy
+// 7. po wcisnieciu przycisku next, w momencie kiedy jest ostatni element slidera, wstaw 1 zdjecie
+// 8. po wcisnieciu przycisku prev, w momencie kiedy jest pierwszy element slidera, wstaw ostatnie zdjecie
+const images = [
+    'https://images.pexels.com/photos/776078/pexels-photo-776078.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ18IG7hjcPiMneoyJIOiTLc_0gHS2Zklc_Di1DTMSHGZ4Mv3I_Vf7_rnG2TUTEKCToVe4&usqp=CAU',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtZfnQBnQh6HMzK9nDkKDLgFXJYaH63s0UD4oe-fzlGA5jZdZa_stDFGhy6JmvVVtdv2g&usqp=CAU'
+]
+
+let counter = 0
+
+const imgContainer = document.querySelector('#img__container')
+imgContainer.setAttribute('src', images[counter])
+console.log(images[counter])
+
+const buttonNext = document.querySelector('#button__next')
+buttonNext.addEventListener('click', (e) => {
+    e.preventDefault()
+    counter = counter + 1
+
+    if (counter > images.length - 1) {
+        imgContainer.setAttribute('src', images[0])
+        counter = 0
+    } else {
+        imgContainer.setAttribute('src', images[counter])
+    }
+
+
+})
+
+
+const buttonPrev = document.querySelector('#button__prev')
+buttonPrev.addEventListener('click', (e) => {
+    e.preventDefault()
+    counter = counter - 1
+
+    if (counter < 0) {
+        imgContainer.setAttribute('src', images[images.length - 1])
+        counter = images.length - 1
+    } else {
+        imgContainer.setAttribute('src', images[counter])
+    }
+})
+    // (() => {
+
+
+    //     const imgContainer = document.querySelector('#img__container')
+    //     imgContainer.setAttribute('src', images[counter])
+
+
+
+
+    // })()
