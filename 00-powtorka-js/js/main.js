31f2fcd9b427f620f19e14f637794bef12114628
dziubek89 <dziubek151@interia.pl>
2023-04-02 11:55:16 +0200

    second task

--- 00-powtorka-js/js/main.js
+++ 00-powtorka-js/js/main.js
@@ -30,8 +30,41 @@ const people = [
 const getOldestPerson = (inputArr) => {
 
     const person = inputArr.sort((a, b) => b.age - a.age)[0]
+
     return person.name
 
 }
 
 console.log(getOldestPerson(people))
+
+// Napisz funkcje o nazwie getTheOldestPerson, ktora obliczy, ktora osoba jest najstarsza w podanej tablicy obiektow. Imie osoby najstarszej, wypisz w konsoli
+
+// Gotowe rozwiazanie wrzuc na GIT i wyslij link.
+
+
+  
+  // Zakladam ze najstarsza osoba jest pierwszym elementem tablicy
+  let max = people[0];
+  
+  // Nastepnie przechodze przez kazdy element tablicy
+  for(let person of people) {
+    // Jesli element tablicy, przez ktory przechdoze jest wiekszy od obecnego maxa, zastepuje maxa
+    if(max.age < person.age) {
+      max = person;
+    }
+  }
+  
+  // po wykonaniu petli for, zmienna max zawiera najstarsza osobe
+  console.log(max.name);
+  
+  
+  // Final Output:
+  
+  // Wiktoria
+  
+  
+  
+  
+  // Powtórka JS
+  
+  
---
+++ 00-powtorka-js/js/task2.js
@@ -0,0 +1,59 @@
+
+// Zadanie z powtórki
+
+// 1. Mając tablicę imion, dopisz do niej swoje imie. Nastepnie za pomoca metody tablica.length - 1, wypisz w konsoli swoje imie.
+
+const names20 = ['Damian', 'Pawel', 'Michal']
+
+names20.push("Krzychu")
+console.log(names20)
+
+// 2. Majac obiekt buta, sprawdz czy rozmiar jest wiekszy od 42. Jesli jest, to wypisz w konsoli "To jest duzy but"
+
+const shoe50 = {
+    brand: "nike",
+    color: 'white',
+    size: 40
+}
+
+const isBigger = shoe50.size > 42 ? console.log('To jest duży but') : null
+
+// 3. Majac tablice produktów, oblicz sumę produktów, których kategoria to owoce
+
+const products = [
+    {
+        name: 'jablko',
+        category: 'fruits',
+        price: 2.00
+    },
+    {
+        name: 'kawa',
+        category: 'other',
+        price: 7.25
+    },
+    {
+        name: 'banan',
+        category: 'fruits',
+        price: 4.99
+    }
+]
+
+let sum = 0
+
+
+for (product of products) {
+    if (product.category === 'owoce') {
+        sum = sum + product.price
+    }
+}
+
+console.log(sum)
+
+// 4. Napisz funkcje sumProducts, ktora przyjmie tablice obiektow products z zadania 3, a nastepnie zwroci sume wszystkich produktow
+
+const sumProducts = (prod) => {
+    const sum = prod.reduce((sum, acc) => sum + acc.price, 0)
+    return sum
+}
+
+console.log(sumProducts(products))
